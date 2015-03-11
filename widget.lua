b23dd7f2f7cbb1ce365a723b095063809d30f5d5
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2015-03-11 21:18:16 +0300

    widget.lua (register): Sort submenu items as well

--- widget.lua
+++ widget.lua
@@ -142,6 +142,11 @@ function register (widget)
                              })
 
    local make_dir_menu = function ()
+      -- Sort a table T alfabetically
+      local alfa_sort = function (t)
+         table.sort (t, function (a, b) return a[1] < b[1] end)
+      end
+
       local items = {}
 
       for i,k in pairs (directory) do
@@ -161,10 +166,11 @@ function register (widget)
          }
       end
 
-      table.sort (items,
-                  function (a, b)
-                     return a[1] < b[1]
-                  end)
+      alfa_sort (items)
+
+      for i,k in pairs (items) do
+         alfa_sort (k[2])
+      end
 
       return items
    end
